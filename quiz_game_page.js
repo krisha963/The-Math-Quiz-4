ac98b9c57c7a10719593feafb35f8e8c8c3ad3c9
krisha963 <89096112+krisha963@users.noreply.github.com>
2022-01-22 16:56:32 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -32,7 +32,9 @@ answer_turn = "player2";
 function check() 
 {
     get_answer = document.getElementById("input_check_box").value;
-
+    
+    if(get_answer == actual_answer)
+    {
     if(answer_turn = "player1")
     {
         player1_score = player1_score + 1;
@@ -43,17 +45,27 @@ function check()
         player2_score = player2_score + 1;
         document.getElementById("player2_score").innerHTML = player2_score;
     }
-
+    }
     if(question_turn = "player1")
     {
-        question_turn = player2;
+        question_turn = "player2";
         document.getElementById("player_question").innerHTML = "Question Turn -" + player2_name;
     }
     else
     {
-        question_turn = player1;
+        question_turn = "player1";
         document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name; 
     }
-    
+    if(answer_turn = "player1")
+    {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn -" + player2_name;
+    }
+    else
+    {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn -" + player1_name; 
+    }
+
     document.getElementById("output").innerHTML = "";
 }
